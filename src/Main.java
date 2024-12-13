e9698fafb914988191dbbde3e3c93866d8f58bfa
Dmytriivasylenko <160059389+Dmytriivasylenko@users.noreply.github.com>
2024-12-13 09:42:25 +0100

    home_work 4.1

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,56 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+// SALES_CALCULATION
+public class Main {
+
+    static String productName;
+    static double totalSales;
+    static double dailySales;
+    private final static String CURRENCY = "USD";
+
+
+    public static void main(String[] args) {
+
+        //product n.1
+        productName = "smartphone";
+        totalSales = 1345.34;
+        dailySales = 3500.00;
+
+        // Розрахунок
+
+         totalSales = totalSales * dailySales;
+         dailySales = totalSales / dailySales;
+
+        // Виведення через printf().
+        // %n - перенесення на інший рядок.
+        // %s - String значення.
+        // %.2f - float/double значення з округленням
+        // до двох знаків після десяткового роздільника.
+
+        System.out.printf("Product No 1: %s,%n total sales for 5 days is %s %.2f,%n sales by day is %s %.2f.%n",
+                productName, CURRENCY, totalSales, CURRENCY, dailySales);
+
+        //product n.2
+
+        productName = "laptop";
+        totalSales = 10486.85;
+        dailySales = 1498.12;
+
+        // Розрахунок
+
+        totalSales = totalSales * dailySales;
+        dailySales = totalSales / dailySales;
+
+        // Виведення через printf().
+        // %n - перенесення на інший рядок.
+        // %s - String значення.
+        // %.2f - float/double значення з округленням
+        // до двох знаків після десяткового роздільника.
+
+        System.out.printf("Product No 2: %s,%n total sales for 14 days is %s %.2f,%n sales by day is %s %.2f.%n",
+                productName, CURRENCY, totalSales, CURRENCY, dailySales);
+
+
+
+
     }
 }
